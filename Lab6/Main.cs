7979c94ecc9f6a9ae172e6f59fab00bbbe60f318
Dmitry Frolov <frdima19@gmail.com>
2020-09-12 16:03:19 +0700

    Key loading and displaying

--- Lab6/Main.cs
+++ Lab6/Main.cs
@@ -12,13 +12,26 @@ namespace Lab6
 	{
 	public partial class Main : Form
 		{
+		Key loadedKey;
+		public Key LoadedKey { get { return loadedKey; } set
+				{
+				loadedKey = value;
+				displayKey(loadedKey);
+				}
+			}
 
 		public Main ()
 			{
 			InitializeComponent();
+			// настройка диалога
+			openFileDialog1.DefaultExt = "key";
+			openFileDialog1.Filter = "USB Key (*.key)|*.key";
+
+			// запуск обсервера
 			USBObserver.Instance.runAutoUpdate();
 			USBObserver.Instance.UpdateDevices += updateDevices;
 
+			// запуск логирования
 			USBLogger.Instance.LoadLogs();
 			USBLogger.Instance.AddLog($"Application started [{DateTime.Now}]");
 			logBox.Text = String.Join("\r\n", USBLogger.Instance.Loggs);
@@ -41,6 +54,15 @@ namespace Lab6
 				textBox1.Text = String.Join("\r\n", newDevices);
 			}
 
+		void displayKey (Key key)
+			{
+			userBox.Text = key.User;
+			serialBox.Text = key.SerialNumber;
+			vendorBox.Text = key.KeyCreator;
+			createdBox.Text = key.CreatedDate.ToString();
+			untillBox.Text = key.UntilDate.ToString();
+			}
+
 		private void createKey_Click (object sender, EventArgs e)
 			{
 			new CreateKey().Show();
@@ -53,6 +75,18 @@ namespace Lab6
 
 			USBLogger.Instance.AddLog($"Application exited [{DateTime.Now}]");
 			USBLogger.Instance.WriteLogs();
+			}
+
+		private void button1_Click (object sender, EventArgs e)
+			{
+			if (openFileDialog1.ShowDialog() == DialogResult.OK)
+				{
+				var path = openFileDialog1.FileName;
+				var key = KeyFactory.ReadKey(path);
+				if ( key != null )
+					LoadedKey = (Key)key;
+				}
+			
 			}
 		}
 	}
