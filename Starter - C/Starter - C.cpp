ccf20ba1f2826103aec19671ad028beba16854c6
I0Andrew0I <43104794+I0Andrew0I@users.noreply.github.com>
2020-11-14 20:43:06 +0700

    Add csharp and c starters

--- Starter - C/Starter - C.cpp
+++ Starter - C/Starter - C.cpp
@@ -1,507 +1,111 @@
-﻿// dll_works_C.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
-#include "stdfax.h"
+﻿#include "stdfax.h"
 
 #include <iostream>
 #include <Windows.h>
 #include <time.h>
-
+#include <float.h>
 
 using namespace std;
 
-typedef double(__cdecl  *Found_max_in_mass)(double*, double*, int, int);
-typedef double(__cdecl  *Found_Min)(double*, double*, int, int);
-typedef double(__cdecl  *Found_Max_in_dual_mass)(double**, int, int);
+typedef double(__cdecl  *VectorType)(double*, double*, int);
+typedef double(__cdecl  *MatrixType)(double**, int, int);
 
-
-double My_function_max (double *mas_1, double *mas_2, int N_1, int N_2)
-{
-	double value_max = mas_1[0];
-
-	for (int i = 0; i < N_1; i++)
+void callDll(CONST WCHAR* dllname) {
+	HINSTANCE hinstLib = LoadLibrary(dllname);
+	if (hinstLib == NULL)
 	{
-		if (value_max < mas_1[i])
-		{
-			value_max = mas_1[i];
-		}
+		cout << "ERROR: unable to load DLL:" << dllname << endl;
+		return;
+	}
+	cout << "DLL: " << dllname << endl;
+	VectorType funct1 = (VectorType)GetProcAddress(hinstLib, "getMinRangeOfVector");
+	VectorType funct2 = (VectorType)GetProcAddress(hinstLib, "getStandardDeviation");
+	MatrixType funct3 = (MatrixType)GetProcAddress(hinstLib, "getAvgValue");
+
+	LARGE_INTEGER FbeginCount, FendCount, Ffrequence;
+
+	//размерность массива
+	int N = 10000;
+	//размерность двухмерного массива
+	int N2 = 100;
+	//Кол-во замеров
+	int countIterations = 10;
+
+	double minTime, maxTime, summTime, avgTime;
+	double* array1 = new double[N];
+	double* array2 = new double[N];
+	for (int i = 0; i < N; i++)
+	{
+		array1[i] = rand() % 1000 + 1;
+		array2[i] = rand() % 1000 + 1;
 	}
 
-	for (int i = 0; i < N_2; i++)
+	minTime = DBL_MAX;
+	maxTime = -DBL_MAX;
+	summTime = 0;
+	for (int count = 0; count < N; count++)
 	{
-		if (value_max < mas_2[i])
+		QueryPerformanceFrequency(&Ffrequence);
+		QueryPerformanceCounter(&FbeginCount);
+		funct1(array1, array2, N);
+		QueryPerformanceCounter(&FendCount);
+		double time = (double)((FendCount.QuadPart - FbeginCount.QuadPart)) * 1000 / Ffrequence.QuadPart;
+		summTime += time;
+		if (minTime > time) minTime = time;
+		if (maxTime < time) maxTime = time;
+	}
+	avgTime = summTime / countIterations;
+	cout << "Среднее, минимум, максимум"<< endl << avgTime << " " << minTime << " " << maxTime << endl;
+
+	minTime = DBL_MAX;
+	maxTime = -DBL_MAX;
+	summTime = 0;
+	for (int count = 0; count < N; count++)
+	{
+		QueryPerformanceFrequency(&Ffrequence);
+		QueryPerformanceCounter(&FbeginCount);
+		funct2(array1, array2, N);
+		QueryPerformanceCounter(&FendCount);
+		double time = (double)((FendCount.QuadPart - FbeginCount.QuadPart)) * 1000 / Ffrequence.QuadPart;
+		summTime += time;
+		if (minTime > time) minTime = time;
+		if (maxTime < time) maxTime = time;
+	}
+	avgTime = summTime / countIterations;
+	cout << "Среднее, минимум, максимум" << endl << avgTime << " " << minTime << " " << maxTime << endl;
+
+	double** array3 = new double*[N];
+	for (int i = 0; i < N2; i++)
+	{
+		array3[i] = new double[N2];
+		for (int j = 0; j < N2; j++)
 		{
-			value_max = mas_2[i];
+			array3[i][j] = rand() % 1000 + 1;
 		}
 	}
-
-	return value_max;
-
+	minTime = DBL_MAX;
+	maxTime = -DBL_MAX;
+	summTime = 0;
+	for (int count = 0; count < N; count++)
+	{
+		QueryPerformanceFrequency(&Ffrequence);
+		QueryPerformanceCounter(&FbeginCount);
+		funct3(array3, N2, N2);
+		QueryPerformanceCounter(&FendCount);
+		double time = (double)((FendCount.QuadPart - FbeginCount.QuadPart)) * 1000 / Ffrequence.QuadPart;
+		summTime += time;
+		if (minTime > time) minTime = time;
+		if (maxTime < time) maxTime = time;
+	}
+	avgTime = summTime / countIterations;
+	cout << "Среднее, минимум, максимум" << endl << avgTime << " " << minTime << " " << maxTime << endl;
 }
-
-//ВЫВОД ВРЕМЕНИ
-void Time_vyvod(double *mas_1, int N_1)
-{
-	double time_max = mas_1[0];
-	double time_min = mas_1[0];
-	double time_avg = 0;
-
-	for (int i = 0; i < N_1; i++)
-	{
-		if (time_max < mas_1[i])
-		{
-			time_max = mas_1[i];
-		}
-
-		if (time_min > mas_1[i])
-		{
-			time_min = mas_1[i];
-		}
-
-		time_avg = time_avg + mas_1[i];
-
-	}
-
-	time_avg = time_avg / 50;
-
-	cout << "time_max="<<time_max<<"\t" << "time_avg=" <<time_avg<< "\t" <<"time_min="<<time_min<<endl;
-
-
-}
-
 
 int main()
 {
 	setlocale(0, "");
 	srand(time(0));
-
-	Found_max_in_mass Max_in_mass;
-	Found_Min Min_in_mass;
-	Found_Max_in_dual_mass Max_in_dual_mass;
-
-	//размерность массива
-	int N = 100000;
-	//размерность двухмерного массива
-	int N_1 = 500;
-	//Кол-во замеров
-	int time_N = 50;
-	//массив со временем
-	double *mass_time_max = new double[time_N];
-
-
-	double *mass_1=new double[N];
-	double *mass_2=new double[N];
-	for (int i = 0; i < N; i++)
-	{
-		mass_1[i]= rand() % 1000 + 1;
-		mass_2[i] = rand() % 1000 + 1;
-	}
-
-
-	cout << "МАССИВЫ СОЗДАНЫ" << endl;
-
-	//double **mass_3=new double *[N_1];
-	//for (int i = 1; i < N_1; i++)
-	//{
-	//	mass_3[i] = new double [N_1];
-	//}
-
-	//for (int i = 0; i < N_1; i++)
-	//{
-	//	for (int j = 0; j < N_1; j++)
-	//	{
-	//		mass_3[i][j] = i*j;
-	//	}
-	//}
-
-	cout << "МАССИВЫ СОЗДАНЫ" << endl;
-
-	//for (int i = 0; i < N; i++)
-	//{
-	//	cout << "mass_1[" << i << "]= " << mass_1[i] <<"\t"<<"mass_2["<<i<<"]= "<<mass_2[i]<< endl;
-	//}
-
-	// Объявление переменных для замера времени
-	LARGE_INTEGER FbeginCount, FendCount, Ffrequence;
-	double Ftime;
-	
-	HINSTANCE hinstLib = LoadLibrary(TEXT("dll_sampel_C.dll"));
-	double result;
-
-	//ИСПОЛЬЗОВАНИЕ VS DLL
-	if (hinstLib == NULL) 
-	{
-		cout << "ERROR: unable to load DLL: dll_sampel_C.dll\n";
-
-	}
-	else
-	{
-		cout << "library (dll_sampel_C.dll) loaded\n";
-
-		//1-АЯ ФУНКЦИЯ
-		Max_in_mass = (Found_max_in_mass)GetProcAddress(hinstLib, "Found_Max");		
-		if (Max_in_mass != NULL) 
-		{
-			for (int count = 0; count < 50; count++)
-			{
-				// Замер времени начала (до выполнения действий)
-				QueryPerformanceFrequency(&Ffrequence);
-				QueryPerformanceCounter(&FbeginCount);
-
-				//ВЫЗОВ ФУНКЦИИ
-				result = Max_in_mass(mass_1, mass_2, N, N);
-				
-				/// Выполнение некоторых вычислений (вызов функций из dll)
-	// Замер времени окончания (после выполнения действий)
-				QueryPerformanceCounter(&FendCount);
-				Ftime = ((FendCount.QuadPart - FbeginCount.QuadPart) / (double)Ffrequence.QuadPart) * 1000;
-
-				mass_time_max[count] = Ftime;
-			}
-			cout << "Время выполнения функции Found_Max из dll_sampel_C.dll :" << endl;
-			Time_vyvod(mass_time_max, time_N);
-		}
-		else
-			cout << "ERROR: unable to find DLL function (Found_Max)\n";
-
-		//2-АЯ ФУНКЦИЯ
-		Min_in_mass = (Found_Min)GetProcAddress(hinstLib, "Found_Min");
-		if (Min_in_mass != NULL)
-		{
-			for (int count = 0; count < 50; count++)
-			{
-				// Замер времени начала (до выполнения действий)
-				QueryPerformanceFrequency(&Ffrequence);
-				QueryPerformanceCounter(&FbeginCount);
-
-				//ВЫЗОВ ФУНКЦИИ
-				result = Min_in_mass(mass_1, mass_2, N, N);
-
-				/// Выполнение некоторых вычислений (вызов функций из dll)
-	// Замер времени окончания (после выполнения действий)
-				QueryPerformanceCounter(&FendCount);
-				Ftime = ((FendCount.QuadPart - FbeginCount.QuadPart) / (double)Ffrequence.QuadPart) * 1000;
-
-				mass_time_max[count] = Ftime;
-			}
-			cout << "Время выполнения функции Found_Min из dll_sampel_C.dll :" << endl;
-			Time_vyvod(mass_time_max, time_N);
-		}
-		else
-			cout << "ERROR: unable to find DLL function (Found_Min)\n";
-
-		//delete mass_1;
-		//delete mass_2;
-
-
-		//double **mass_3 = new double*[N_1];
-		//for (int i = 0; i < N_1; i++)
-		//{
-		//	mass_3[i] = new double[N_1];
-		//}
-		//for (int i = 0; i < N_1; i++)
-		//{
-		//	for (int j = 0; j < N_1; i++)
-		//	{
-		//		mass_3[i][j] = rand() % 1000 + 1;
-		//	}
-		//}
-
-
-
-		//3-АЯ ФУНКЦИЯ
-		Max_in_dual_mass = (Found_Max_in_dual_mass)GetProcAddress(hinstLib, "Found_Max_in_dual_mass");
-		if (Max_in_dual_mass != NULL)
-		{
-			for (int count = 0; count < 50; count++)
-			{
-				// Замер времени начала (до выполнения действий)
-				QueryPerformanceFrequency(&Ffrequence);
-				QueryPerformanceCounter(&FbeginCount);
-
-				//ВЫЗОВ ФУНКЦИИ
-//				result = Max_in_dual_mass(mass_3, N_1, N_1);
-
-				/// Выполнение некоторых вычислений (вызов функций из dll)
-	// Замер времени окончания (после выполнения действий)
-				QueryPerformanceCounter(&FendCount);
-				Ftime = ((FendCount.QuadPart - FbeginCount.QuadPart) / (double)Ffrequence.QuadPart) * 1000;
-
-				mass_time_max[count] = Ftime;
-			}
-			cout << "Время выполнения функции Found_Max_in_dual_mass из dll_sampel_C.dll :" << endl;
-			Time_vyvod(mass_time_max, time_N);
-		}
-		else
-			cout << "ERROR: unable to find DLL function (Found_Max_in_dual_mass)\n";
-
-		//for (int i = 0; i < N_1; i++)
-		//{
-		//	delete mass_3[i];
-		//}
-
-		//delete mass_3;
-
-
-	}
-
-	FreeLibrary(hinstLib);
-
-
-	//ИСПОЛЬЗОВАНИЕ RAD C++ DLL
-	HINSTANCE hinstLib_RAD_C = LoadLibrary(TEXT("Project_RAD_DLL.dll"));
-	if (hinstLib_RAD_C == NULL)
-	{
-		cout << "ERROR: unable to load DLL: Project_RAD_DLL.dll\n";
-
-	}
-	else
-	{
-		cout << "library (Project_RAD_DLL.dll) loaded\n";
-
-
-		//double *mass_1 = new double[N];
-		//double *mass_2 = new double[N];
-		//for (int i = 0; i < N; i++)
-		//{
-		//	mass_1[i] = rand() % 1000 + 1;
-		//	mass_2[i] = rand() % 1000 + 1;
-		//}
-
-		//1-АЯ ФУНКЦИЯ
-		Max_in_mass = (Found_max_in_mass)GetProcAddress(hinstLib_RAD_C, "_Found_Max");
-		if (Max_in_mass != NULL)
-		{
-			for (int count = 0; count < 50; count++)
-			{
-				// Замер времени начала (до выполнения действий)
-				QueryPerformanceFrequency(&Ffrequence);
-				QueryPerformanceCounter(&FbeginCount);
-
-				//ВЫЗОВ ФУНКЦИИ
-				result = Max_in_mass(mass_1, mass_2, N, N);
-
-				/// Выполнение некоторых вычислений (вызов функций из dll)
-	// Замер времени окончания (после выполнения действий)
-				QueryPerformanceCounter(&FendCount);
-				Ftime = ((FendCount.QuadPart - FbeginCount.QuadPart) / (double)Ffrequence.QuadPart) * 1000;
-
-				mass_time_max[count] = Ftime;
-			}
-			cout << "Время выполнения функции Found_Max из Project_RAD_DLL.dll :" << endl;
-			Time_vyvod(mass_time_max, time_N);
-		}
-		else
-			cout << "ERROR: unable to find DLL function (Found_Max)\n";
-
-		//2-АЯ ФУНКЦИЯ
-		Min_in_mass = (Found_Min)GetProcAddress(hinstLib_RAD_C, "_Found_Min");
-		if (Min_in_mass != NULL)
-		{
-			for (int count = 0; count < 50; count++)
-			{
-				// Замер времени начала (до выполнения действий)
-				QueryPerformanceFrequency(&Ffrequence);
-				QueryPerformanceCounter(&FbeginCount);
-
-				//ВЫЗОВ ФУНКЦИИ
-				result = Min_in_mass(mass_1, mass_2, N, N);
-
-				/// Выполнение некоторых вычислений (вызов функций из dll)
-	// Замер времени окончания (после выполнения действий)
-				QueryPerformanceCounter(&FendCount);
-				Ftime = ((FendCount.QuadPart - FbeginCount.QuadPart) / (double)Ffrequence.QuadPart) * 1000;
-
-				mass_time_max[count] = Ftime;
-			}
-			cout << "Время выполнения функции Found_Min из Project_RAD_DLL.dll :" << endl;
-			Time_vyvod(mass_time_max, time_N);
-		}
-		else
-			cout << "ERROR: unable to find DLL function (Found_Min)\n";
-
-		//delete mass_1;
-		//delete mass_2;
-
-		//double **mass_3 = new double*[N_1];
-		//for (int i = 0; i < N_1; i++)
-		//{
-		//	mass_3[i] = new double[N_1];
-		//}
-		//for (int i = 0; i < N_1; i++)
-		//{
-		//	for (int j = 0; j < N_1; i++)
-		//	{
-		//		mass_3[i][j] = rand() % 1000 + 1;
-		//	}
-		//}
-
-		//3-АЯ ФУНКЦИЯ
-		Max_in_dual_mass = (Found_Max_in_dual_mass)GetProcAddress(hinstLib_RAD_C, "_Found_Max_in_dual_mass");
-		if (Max_in_dual_mass != NULL)
-		{
-			for (int count = 0; count < 50; count++)
-			{
-				// Замер времени начала (до выполнения действий)
-				QueryPerformanceFrequency(&Ffrequence);
-				QueryPerformanceCounter(&FbeginCount);
-
-				//ВЫЗОВ ФУНКЦИИ
-//				result = Max_in_dual_mass(mass_3, N_1, N_1);
-
-				/// Выполнение некоторых вычислений (вызов функций из dll)
-	// Замер времени окончания (после выполнения действий)
-				QueryPerformanceCounter(&FendCount);
-				Ftime = ((FendCount.QuadPart - FbeginCount.QuadPart) / (double)Ffrequence.QuadPart) * 1000;
-
-				mass_time_max[count] = Ftime;
-			}
-			cout << "Время выполнения функции Found_Max_in_dual_mass из Project_RAD_DLL.dll :" << endl;
-			Time_vyvod(mass_time_max, time_N);
-		}
-		else
-			cout << "ERROR: unable to find DLL function (Found_Max_in_dual_mass)\n";
-
-
-		//for (int i = 0; i < N_1; i++)
-		//{
-		//	delete mass_3[i];
-		//}
-
-		//delete mass_3;
-	}
-
-	FreeLibrary(hinstLib_RAD_C);
-
-
-	//ИСПОЛЬЗОВАНИЕ RAD Delphi DLL
-	HINSTANCE hinstLib_RAD_Delphi = LoadLibrary(TEXT("Project_Delphi_DLL.dll"));
-	if (hinstLib_RAD_Delphi == NULL)
-	{
-		cout << "ERROR: unable to load DLL: Project_Delphi_DLL.dll\n";
-
-	}
-	else
-	{
-		cout << "library (Project_Delphi_DLL.dll) loaded\n";
-
-		//double *mass_1 = new double[N];
-		//double *mass_2 = new double[N];
-		//for (int i = 0; i < N; i++)
-		//{
-		//	mass_1[i] = rand() % 1000 + 1;
-		//	mass_2[i] = rand() % 1000 + 1;
-		//}
-
-
-		//1-АЯ ФУНКЦИЯ
-		Max_in_mass = (Found_max_in_mass)GetProcAddress(hinstLib_RAD_Delphi, "Found_Max");
-		if (Max_in_mass != NULL)
-		{
-			for (int count = 0; count < 50; count++)
-			{
-				// Замер времени начала (до выполнения действий)
-				QueryPerformanceFrequency(&Ffrequence);
-				QueryPerformanceCounter(&FbeginCount);
-
-				//ВЫЗОВ ФУНКЦИИ
-				result = Max_in_mass(mass_1, mass_2, N, N);
-
-				/// Выполнение некоторых вычислений (вызов функций из dll)
-	// Замер времени окончания (после выполнения действий)
-				QueryPerformanceCounter(&FendCount);
-				Ftime = ((FendCount.QuadPart - FbeginCount.QuadPart) / (double)Ffrequence.QuadPart) * 1000;
-
-				mass_time_max[count] = Ftime;
-			}
-			cout << "Время выполнения функции Found_Max из Project_Delphi_DLL.dll :" << endl;
-			Time_vyvod(mass_time_max, time_N);
-		}
-		else
-			cout << "ERROR: unable to find DLL function (Found_Max)\n";
-
-		//2-АЯ ФУНКЦИЯ
-		Min_in_mass = (Found_Min)GetProcAddress(hinstLib_RAD_Delphi, "Found_Min");
-		if (Min_in_mass != NULL)
-		{
-			for (int count = 0; count < 50; count++)
-			{
-				// Замер времени начала (до выполнения действий)
-				QueryPerformanceFrequency(&Ffrequence);
-				QueryPerformanceCounter(&FbeginCount);
-
-				//ВЫЗОВ ФУНКЦИИ
-				result = Min_in_mass(mass_1, mass_2, N, N);
-
-				/// Выполнение некоторых вычислений (вызов функций из dll)
-	// Замер времени окончания (после выполнения действий)
-				QueryPerformanceCounter(&FendCount);
-				Ftime = ((FendCount.QuadPart - FbeginCount.QuadPart) / (double)Ffrequence.QuadPart) * 1000;
-
-				mass_time_max[count] = Ftime;
-			}
-			cout << "Время выполнения функции Found_Min из Project_Delphi_DLL.dll :" << endl;
-			Time_vyvod(mass_time_max, time_N);
-		}
-		else
-			cout << "ERROR: unable to find DLL function (Found_Min)\n";
-
-
-		//delete mass_1;
-		//delete mass_2;
-
-
-		//double **mass_3 = new double*[N_1];
-		//for (int i = 0; i < N_1; i++)
-		//{
-		//	mass_3[i] = new double[N_1];
-		//}
-		//for (int i = 0; i < N_1; i++)
-		//{
-		//	for (int j = 0; j < N_1; i++)
-		//	{
-		//		mass_3[i][j] = rand() % 1000 + 1;
-		//	}
-		//}
-
-
-		//3-АЯ ФУНКЦИЯ
-		Max_in_dual_mass = (Found_Max_in_dual_mass)GetProcAddress(hinstLib_RAD_Delphi, "Found_Max_in_dual_mass");
-		if (Max_in_dual_mass != NULL)
-		{
-			for (int count = 0; count < 50; count++)
-			{
-				// Замер времени начала (до выполнения действий)
-				QueryPerformanceFrequency(&Ffrequence);
-				QueryPerformanceCounter(&FbeginCount);
-
-				//ВЫЗОВ ФУНКЦИИ
-//				result = Max_in_dual_mass(mass_3, N_1, N_1);
-
-				/// Выполнение некоторых вычислений (вызов функций из dll)
-	// Замер времени окончания (после выполнения действий)
-				QueryPerformanceCounter(&FendCount);
-				Ftime = ((FendCount.QuadPart - FbeginCount.QuadPart) / (double)Ffrequence.QuadPart) * 1000;
-
-				mass_time_max[count] = Ftime;
-			}
-			cout << "Время выполнения функции Found_Max_in_dual_mass из Project_Delphi_DLL.dll :" << endl;
-			Time_vyvod(mass_time_max, time_N);
-		}
-		else
-			cout << "ERROR: unable to find DLL function (Found_Max_in_dual_mass)\n";
-	
-
-		//for (int i = 0; i < N_1; i++)
-		//{
-		//	delete mass_3[i];
-		//}
-
-		//delete mass_3;
-
-
-	}
-
-	FreeLibrary(hinstLib_RAD_Delphi);
+	callDll(TEXT("DLL - C.dll"));
+	callDll(TEXT("DLL - RAD C Builder.dll"));
+	callDll(TEXT("DLL - RAD C Delphi.dll"));
 }
-
--- Starter - CSharp/Form1.cs
+++ Starter - CSharp/Form1.cs
@@ -18,12 +18,11 @@ namespace Starter___CSharp
 			InitializeComponent();
 		}
 
-
 		#region importsDLL
-		//[DllImport("Kernel32.dll")]
-		//private static extern bool QueryPerformanceCounter(out long lpPerformanceCount);
-		//[DllImport("Kernel32.dll")]
-		//private static extern bool QueryPerformanceFrequency(out long lpFrequency);
+		[DllImport("Kernel32.dll")]
+		private static extern bool QueryPerformanceCounter(out long lpPerformanceCount);
+		[DllImport("Kernel32.dll")]
+		private static extern bool QueryPerformanceFrequency(out long lpFrequency);
 
 		[DllImport("kernel32.dll", EntryPoint = "LoadLibrary")]
 		static extern int LoadLibrary(
@@ -43,37 +42,34 @@ namespace Starter___CSharp
 		public delegate double MatrixType(IntPtr[] array1, int size1, int size2);
 
 		[UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-		public delegate VoidVoid VoidVoid();
-
+		public delegate void VoidVoid();
 		#endregion importsDLL
-
 
 		private void button1_Click(object sender, EventArgs e)
 		{
 			callDll("DLL - C.dll");
+			callDll("DLL - RAD C Builder.dll");
+			callDll("DLL - RAD C Delphi.dll");
 		}
 
-
-		int size = 2000000;
+		//int size = 10000000;
+		int size = 10000;
 
 		void callDll(string dllPath)
 		{
 			int pointerDll = LoadLibrary(dllPath);
 
-			IntPtr getProc = GetProcAddress((pointerDll), "test");
-			VoidVoid test = Marshal.GetDelegateForFunctionPointer<VoidVoid>(getProc);
-
 			VectorType funct1 = getVectorFunctInDll(pointerDll, "getMinRangeOfVector");
 			string timesString1 = getTimesStringVector(funct1);
 			textBox1.AppendText(timesString1 + "\r\n");
 
-			//VectorType funct2 = getVectorFunctInDll(pointerDll, "getStandardDeviation");
-			//string timesString2 = getTimesStringVector(funct2);
-			//textBox1.AppendText(timesString1 + "\r\n");
+			VectorType funct2 = getVectorFunctInDll(pointerDll, "getStandardDeviation");
+			string timesString2 = getTimesStringVector(funct2);
+			textBox1.AppendText(timesString2 + "\r\n");
 
-			//MatrixType funct3 = getMatrixFunctInDll(pointerDll, "getAvgValue");
-			//string timesString3 = getTimesStringMatrix(funct3);
-			//textBox1.AppendText(timesString1 + "\r\n");
+			MatrixType funct3 = getMatrixFunctInDll(pointerDll, "getAvgValue");
+			string timesString3 = getTimesStringMatrix(funct3);
+			textBox1.AppendText(timesString3 + "\r\n");
 
 			FreeLibrary(pointerDll);
 		}
@@ -104,7 +100,6 @@ namespace Starter___CSharp
 			}
 		}
 
-
 		double[] getRandomArray(int size)
 		{
 			double[] array = new double[size];
@@ -115,7 +110,6 @@ namespace Starter___CSharp
 			}
 			return array;
 		}
-
 
 		string getTimesStringVector(VectorType funct)
 		{
@@ -128,21 +122,13 @@ namespace Starter___CSharp
 			array1 = getRandomArray(size);
 			array2 = getRandomArray(size);
 
-			//IntPtr array1Ptr = Marshal.AllocCoTaskMem(sizeof(double) * array1.Length);
-			//Marshal.Copy(array1, 0, array1Ptr, array1.Length);
-
-			//IntPtr array2Ptr = Marshal.AllocCoTaskMem(sizeof(double) * array2.Length);
-			//Marshal.Copy(array2, 0, array2Ptr, array2.Length);
-
 			for (int i = 0; i < countIterations; i++)
 			{
-				
-				//QueryPerformanceFrequency(out Ffrequence);
-				//QueryPerformanceCounter(out FbeginCount);
-				funct(array1, array2, size);
-				//QueryPerformanceCounter(out FendCount);
-				//double time = (FendCount - FbeginCount) / (double)Ffrequence;
-				double time = 0;
+				QueryPerformanceFrequency(out Ffrequence);
+				QueryPerformanceCounter(out FbeginCount);
+				double value = funct(array1, array2, size);
+				QueryPerformanceCounter(out FendCount);
+				double time = (double)((FendCount - FbeginCount)) * 1000 / Ffrequence;
 				summTime += time;
 				if (minTime > time) minTime = time;
 				if (maxTime < time) maxTime = time;
@@ -152,40 +138,42 @@ namespace Starter___CSharp
 			return "Среднее, минимум, максимум\r\n" + avgTime.ToString() + " " + minTime.ToString() + " " + maxTime.ToString() + "\r\n";
 		}
 
-		//string getTimesStringMatrix(MatrixType funct)
-		//{
-		//	long FbeginCount, FendCount, Ffrequence;
-		//	double minTime = double.MaxValue;
-		//	double maxTime = double.MinValue;
-		//	double summTime = 0;
-		//	int countIterations = 10;
-		//	IntPtr[] array1;
-		//	unsafe
-		//	{
-		//		array1 = new IntPtr[size];
-		//		for (int k = 0; k < size; k++)
-		//		{
-		//			fixed (void* ptr = getRandomArray(size))
-		//			{
-		//				array1[k] = new IntPtr(ptr);
-		//			}
-		//		}
-		//	}
-		//	for (int i = 0; i < countIterations; i++)
-		//	{
-		//		//QueryPerformanceFrequency(out Ffrequence);
-		//		//QueryPerformanceCounter(out FbeginCount);
-		//		funct(array1, size, size);
-		//		//QueryPerformanceCounter(out FendCount);
-		//		double time = (FendCount - FbeginCount) / (double)Ffrequence;
+		string getTimesStringMatrix(MatrixType funct)
+		{
+			long FbeginCount, FendCount, Ffrequence;
+			double minTime = double.MaxValue;
+			double maxTime = double.MinValue;
+			double summTime = 0;
+			int countIterations = 10;
+			IntPtr[] array1;
+			int localSize = size / 10000;
+			unsafe
+			{
+				array1 = new IntPtr[localSize];
+				for (int k = 0; k < localSize; k++)
+				{
+					double[] arr = getRandomArray(localSize);
+					fixed (void* ptr = arr)
+					{
+						array1[k] = new IntPtr(ptr);
+					}
+				}
+			}
+			for (int i = 0; i < countIterations; i++)
+			{
+				QueryPerformanceFrequency(out Ffrequence);
+				QueryPerformanceCounter(out FbeginCount);
+				double value = funct(array1, localSize, localSize);
+				QueryPerformanceCounter(out FendCount);
 
-		//		summTime += time;
-		//		if (minTime > time) minTime = time;
-		//		if (maxTime < time) maxTime = time;
-		//	}
-		//	double avgTime = summTime / countIterations;
+				double time = (double)((FendCount - FbeginCount)) * 1000 / Ffrequence;
+				summTime += time;
+				if (minTime > time) minTime = time;
+				if (maxTime < time) maxTime = time;
+			}
+			double avgTime = summTime / countIterations;
 
-		//	return "Среднее, минимум, максимум\r\n" + avgTime.ToString() + " " + minTime.ToString() + " " + maxTime.ToString() + "\r\n";
-		//}
+			return "Среднее, минимум, максимум\r\n" + avgTime.ToString() + " " + minTime.ToString() + " " + maxTime.ToString() + "\r\n";
+		}
 	}
 }
